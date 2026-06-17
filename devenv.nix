f43ebfbfb7e6569f9de710fbfa38950a92923d8f
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2026-06-17 19:13:39 -0400

    feat(devenv.yaml): add devenv.yaml file to manage environment variables and packages for development environment fix(devenv.nix): correct the indentation of the env variable assignment in server.ts

---
+++ devenv.nix
@@ -0,0 +1,82 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env = {
+    GREET = "devenv";
+    OCO_AI_PROVIDER = "ollama";
+    OCO_PROMPT_MODULE = "conventional-commit"; 
+    OCO_MODEL = "qwen2.5-coder:3b";
+  };
+
+  dotenv.enable = true;
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    git-cliff
+    opencommit
+    jupyter
+    nixpkgs-fmt
+  ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    package = pkgs.python312;
+    lsp.enable = true;
+    venv.enable = true;
+    
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    hello
+    git --version
+    export OCO_API_CUSTOM_HEADERS="{\"Authorization\": \"Bearer $OLLAMA_API_KEY\"}"
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+# https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+  # 1. The Jupyter Notebook Clear Output Hook (Local/Custom)
+  jupyter-nb-clear-output = {
+    enable = true;
+    name = "jupyter-nb-clear-output";
+    entry = "jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace";
+    files = "\\.ipynb$";
+    stages = [ "pre-commit" ];
+  };
+
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
